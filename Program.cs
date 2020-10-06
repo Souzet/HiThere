0b243acd9404633cbeb28844da492057e0c99167
souzet tadros <souzettadros@gmail.com>
2020-10-06 15:12:14 +0200

    Start

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HiThere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
